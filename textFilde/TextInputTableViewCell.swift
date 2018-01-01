67e2e4d83c9a122fd2696aabb651ed8968acb18e
vd <vdbazinga79@gmail.com>
2018-01-01 12:45:43 +0530

    ui view

---
+++ textFilde/TextInputTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TextInputTableViewCell.swift
+//  textFilde
+//
+//  Created by darshan on 01/01/18.
+//  Copyright © 2018 darshan. All rights reserved.
+//
+
+import UIKit
+
+class TextInputTableViewCell: UITableViewCell{
+
+   
+    @IBOutlet var number1: UITextField!
+    
+    
+    @IBOutlet var number2: UITextField!
+    
+
+    @IBOutlet var addButton: UIButton!
+}
--- textFilde/ViewController.swift
+++ textFilde/ViewController.swift
@@ -8,12 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate,UITextFieldDelegate {
+    
+    @IBOutlet var inputText: UITextField!
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var numberRows : Int = 0
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return numberRows//number of cell we want
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+         self.inputText.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        //textField code
+        numberRows = Int(inputText.text!)!
+        inputText.resignFirstResponder()  //if desired
+        tableView.reloadData()
+        return true
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
